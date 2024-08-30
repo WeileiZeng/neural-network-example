a51fbe8edbd0b0bb56b975d9ed23e3910b9d126a
Weilei uses root on qlab-gpu-8-1 <weileizeng@zhejianglan.com>
2024-08-30 09:33:47 +0800

    add more test

--- step-function/tri-wave.py
+++ step-function/tri-wave.py
@@ -26,6 +26,7 @@ import torch
 import torch.nn as nn
 
 from torch.autograd import Variable
+pieces = 10
 
 device = 'cuda:7'
 #torch.cuda.set_device(0)
@@ -34,10 +35,12 @@ torch.set_default_device(device)
 #x_data = Variable(torch.Tensor([[1.0], [2.0], [3.0]]))
 #y_data = Variable(torch.Tensor([[2.0], [4.0], [6.0]]))
 
-def get_data(n):
+def get_data(n): 
     n = int(n)
+    #X = torch.rand(n,1)*pieces
     X = torch.rand(n,1)*2
-    y = X.floor() - X + 1
+    y = X.floor() - X + 1 # tri wave
+    #y = X.floor()  # floor 
     return X,y
 def data_test():
     X,y = get_data(10)
@@ -47,35 +50,48 @@ def data_test():
 #exit()
 
 
-x_test,y_test=get_data(1e3)
+x_test,y_test=get_data(1e4)
 print('mean x y',x_test.mean(),y_test.mean())
 #x_test=torch.rand((int(1e5),1))*10 + 1
 #y_test = func(x_test)
 
+#from floor import RegressionModel
+
+def distri(b,parallel):
+    li = [p(x) for p in parallel]
+    _ = torch.stack(li, dim=0)
+    _ = _.squeeze()
+
+    y = b @ _
+    y = y.diagonal()
+    y = y.reshape((y.shape[0],1))
+    #print(y.shape)
+    return y
 class PreClassificationModel(torch.nn.Module):
 
         def __init__(self):                
                 super(PreClassificationModel, self).__init__()
                 l=64
-                out=10
+                out=pieces
                 self.classification = nn.Sequential(
                     nn.Linear(1,l),
-                    nn.BatchNorm1d(l), nn.ReLU(), nn.Dropout(),nn.Linear(l,l),
-                    nn.BatchNorm1d(l), nn.ReLU(), nn.Dropout(),nn.Linear(l,l),
-                    nn.BatchNorm1d(l), nn.ReLU(), nn.Dropout(),nn.Linear(l,l),
-                    nn.BatchNorm1d(l), nn.ReLU(), nn.Dropout(),nn.Linear(l,l),
-                    nn.BatchNorm1d(l), nn.ReLU(), nn.Dropout(),
+                    * ([nn.Sigmoid(),nn.Linear(l,l)]*4),                    
+                    nn.Sigmoid(),
                     nn.Linear(l,out),
                     #nn.BatchNorm1d(10), nn.ReLU(), nn.Dropout(),nn.Linear(10,10),
                     #nn.Dropout(), nn.ReLU(), nn.Linear(10,10),
                     nn.Softmax(),
                     #nn.Sigmoid()
                 )
-                self.parallel = [ nn.Linear(1,1) for _ in range(10) ]
+                self.parallel = [ nn.Linear(1,1) for _ in range(pieces) ]
                 
                 
         def forward(self, x):
                 b = self.classification(x)
+
+                #y = distri(b,self.parallel)
+                #return y
+        
                 #print(b.shape)
                 li = [p(x) for p in self.parallel]
                 _ = torch.stack(li, dim=0)
@@ -93,19 +109,45 @@ class PreClassificationModel(torch.nn.Module):
 def test_model(model):
     # test the classification of the model
     #x = torch.Tensor([[1.0], [2.0], [3.0]])
-    x = torch.range(1,10).reshape((10,1)) -0.3
+    x = torch.arange(0,10).reshape((10,1))/10+0.5
+    #x = torch.range(1,10).reshape((10,1)) -0.3
     #tensor([[0.7000], [1.7000], [2.7000], [3.7000], [4.7000], [5.7000], [6.7000], [7.7000], [8.7000], [9.7000]])
     x = x.to(device)
+    print('x',x.squeeze())
     b = model.classification(x)
-    print(b)
-    values, indices = b.max(dim=1)
+    print('b', b)
+    
+    values, col_indices = b.max(dim=1)
+
+    li = [p(x) for p in model.parallel]
+    _ = torch.stack(li, dim=0)
+    _ = _.squeeze()
+    #print(_.shape)
+    _ = _.transpose(0,1)
+    #print(x.shape)
+    #_a = torch.range(0,9, dtype=torch.long)
+    #_a = torch.range(0,x.shape[0]-1, dtype=torch.long)
+    row_indices = torch.arange(0,x.shape[0], dtype=torch.long)
+    #print('row_indices',row_indices)
+    #print('col_indices',col_indices)
+    #print(_)
+    y_new = _[row_indices,col_indices]
+    #print('x    ',x.squeeze())
+    print('y_new',y_new)
+    
+    #y[row_indices,col_indices]=1
+
+    #model_p=model.parallel[indices]
+    #y = model_p(x)
+    #y = distri(b,model.parrallel)
+    #print(y)
     #print(values)
-    print(indices)
+    print('classfify',col_indices)
     
 our_model = PreClassificationModel()
 print(our_model)
 
-tt
+#tt
 #test_model(our_model)
 #exit()
 
@@ -114,7 +156,7 @@ criterion = torch.nn.MSELoss()
 #optimizer = torch.optim.SGD(our_model.parameters(), lr = 0.0000001)
 optimizer = torch.optim.Adam(our_model.parameters(), lr = 0.0001)
 
-for epoch in range(500000):
+for epoch in range(50000):
         our_model.train()
         x_data,y_data = get_data(16)
         #x_data=torch.rand((int(1e3),1))*10 + 1
@@ -138,9 +180,9 @@ for epoch in range(500000):
                 our_model.eval()
                 pred_y = our_model(x_test)
                 loss_val = criterion(pred_y, y_test)
-                #print(pred_y[:10])
-                #print(y_test[:10])                
-                print(' '*35,' epoch {}, training loss {},        \tvalidation loss {}.    '.format(epoch, loss.item(), loss_val.item()), end='\r' )
+                print('pred_y',pred_y[:10].squeeze())
+                print('y_test',y_test[:10].squeeze())                
+                print(' '*35,' epoch {}, training loss {},        \tvalidation loss {}.    '.format(epoch, loss.item(), loss_val.item()), end='\n' )
                 #input()
                 test_model(our_model)
         
